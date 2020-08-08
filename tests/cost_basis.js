0b7bec251c776f0b3afff6320eb7abcb5b0d603a
dogracer <d@jumphook.com>
2020-08-07 23:20:39 -0500

    Add test0 which happens to fail

--- tests/cost_basis.js
+++ tests/cost_basis.js
@@ -20,7 +20,7 @@
 
 QUnit.helpers( this );
 function testFunctions() {
-  
+  test0_CostBasis();  
   test4_CostBasis();
 }
  
@@ -34,6 +34,82 @@ function doGet( e ) {
   return QUnit.getHtml();
 };
  
+/** 
+ * test0 for function calculateFifo(sheet, lots, sales)
+ */
+function test0_CostBasis() {
+  QUnit.test( "Cost Basis Calculation (FIFO) - test0", function() {
+    // create temp sheet
+    var currentdate = new Date(); 
+    var uniqueSheetName = "test0:" + (currentdate.getMonth()+1) + "/"
+                + currentdate.getDate() + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(uniqueSheetName);
+    
+    // test data for this test case
+    var initialData = [['2017-01-01','0.2','2000','',''],
+                       ['2018-02-01','0.6','6000','',''],
+                       ['2018-02-01','','','0.1','2000'],
+                       ['2018-03-01','','','0.4','8000'],
+                       ['2018-03-02','0.4','4000','',''],
+                       ['2018-03-03','0.8','8000','',''],
+                       ['2018-03-04','0.6','6000','',''],
+                       ['2018-03-05','','','0.1','500'],
+                       ['2018-03-06','','','0.1','1000'],
+                       ['2018-03-07','','','0.1','2000']];
+    
+    for (var i = 0; i < initialData.length; i++) {
+      sheet.getRange('A'+(i+3)+':E'+(i+3)).setValues([initialData[i]]);
+    }
+    
+    // mimic calculateFIFO_() 
+    if (validate(sheet)) {
+    
+      var lots = getLots(sheet);
+      var sales = getSales(sheet);
+    
+      calculateFifo(sheet, lots, sales);
+      
+      // output the current date and time as the time last completed
+      var now = Utilities.formatDate(new Date(), 'CST', 'MMMM dd, yyyy HH:mm');
+      sheet.getRange('I1').setValue('Last calculation succeeded '+now);
+          
+    } else {
+      
+      var now = Utilities.formatDate(new Date(), 'CST', 'MMMM dd, yyyy HH:mm');
+      sheet.getRange('I1').setValue('Data validation failed '+now);
+    }
+    
+    // check if test passed or failed
+    expect(11);
+    equal( sheet.getRange('F3').getValue(), "100% Sold", "Test for Lot Sold In Full Later : Row 3 Status : expected 100% sold" );
+    equal( sheet.getRange('F4').getValue(), "100% Sold", "Test for Lot Sold In Full Later : Row 4 Status : expected 100% sold" );
+
+    equal( sheet.getRange('F5').getValue(), "Long-term", "Test for Long-Term Sale : Row 5 Status : expected long-term 1000 cost basis" );
+    equal( sheet.getRange('G5').getValue(), 1000, "Test for Long-Term Sale : Row 5 Cost Basis : expected long-term 1000 cost basis" );
+    equal( sheet.getRange('H5').getValue(), 1000, "Test for Long-Term Sale : Row 5 Gain(Loss) : expected long-term 1000 gain" );
+    
+    equal( sheet.getRange('F6').getValue(), "Long-term", "Test for Split into Long-Term Sale : Row 6 Status : expected long-term 1000 cost basis" );
+    equal( sheet.getRange('G6').getValue(), 1000, "Test for Split into Long-Term Sale : Row 6 Cost Basis : expected long-term 1000 cost basis" );
+    equal( sheet.getRange('H6').getValue(), 1000, "Test for Split into Long-Term Sale : Row 6 Gain(Loss) : expected long-term 1000 gain" );
+    // also check the NOTE content...
+    // "Split into (rows 6 and 7). Amount of coin sold was 0.40000000, and original amount was $8000.00."
+
+    equal( sheet.getRange('F7').getValue(), "Short-term", "Test for Split into Short-Term Sale : Row 7 Status : expected long-term 1000 cost basis" );
+    equal( sheet.getRange('G7').getValue(), 3000, "Test for Split into Short-Term Sale : Row 7 Cost Basis : expected long-term 3000 cost basis" );
+    equal( sheet.getRange('H7').getValue(), 3000, "Test for Split into Short-Term Sale : Row 7 Gain(Loss) : expected long-term 3000 gain" );
+    // also check the NOTE content...
+    // "Sale split into (rows 6 and 7). Original amount of coin sold was 0.40000000, and original amount was $8000.00."
+
+    // clean up temp sheet
+    SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheet);
+  });
+}
+
+
 /** 
  * test4 for function calculateFifo(sheet, lots, sales)
  */
