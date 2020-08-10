d9e34f44c4361a164b61218292a4579640e829fa
dogracer <d@jumphook.com>
2020-08-09 21:38:16 -0500

    Real data scenario added

--- tests/cost_basis.js
+++ tests/cost_basis.js
@@ -23,6 +23,7 @@ function testFunctions() {
   test0_CostBasis();  
   test4_CostBasis();
   test6_CostBasis();
+  test8_CostBasis();
 }
  
 function doGet( e ) {
@@ -39,7 +40,7 @@ function doGet( e ) {
  * test0 for function calculateFifo(sheet, lots, sales)
  */
 function test0_CostBasis() {
-  QUnit.test( "Cost Basis Calculation (FIFO) - test0", function() {
+  QUnit.test( "test0 - Sample Dataset", function() {
     // create temp sheet
     var currentdate = new Date(); 
     var uniqueSheetName = "test0:" + (currentdate.getMonth()+1) + "/"
@@ -115,7 +116,7 @@ function test0_CostBasis() {
  * test4 for function calculateFifo(sheet, lots, sales)
  */
 function test4_CostBasis() {
-  QUnit.test( "Cost Basis Calculation (FIFO) - test4", function() {
+  QUnit.test( "test4 - Simple Partial Sale", function() {
     // create temp sheet
     var currentdate = new Date(); 
     var uniqueSheetName = "test4:" + (currentdate.getMonth()+1) + "/"
@@ -174,7 +175,7 @@ function test4_CostBasis() {
  * test6 for function calculateFifo(sheet, lots, sales)
  */
 function test6_CostBasis() {
-  QUnit.test( "Cost Basis Calculation (FIFO) - test6", function() {
+  QUnit.test( "test6 - Simple Term Split", function() {
     // create temp sheet
     var currentdate = new Date(); 
     var uniqueSheetName = "test6:" + (currentdate.getMonth()+1) + "/"
@@ -229,6 +230,107 @@ function test6_CostBasis() {
     // also check the NOTE content...
     // "Sale split into (rows 5 and 6). Original amount of coin sold was 2.00000000, and original amount was $4000.00."
     
+    // clean up temp sheet
+    SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheet);
+  });
+}
+
+/** 
+ * test8 for function calculateFifo(sheet, lots, sales)
+ */
+function test8_CostBasis() {
+  QUnit.test( "test8 - Real Data with Term Split - Two Rounds", function() {
+    // create temp sheet
+    var currentdate = new Date(); 
+    var uniqueSheetName = "test8:" + (currentdate.getMonth()+1) + "/"
+                + currentdate.getDate() + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(uniqueSheetName);
+    
+    // test data for this test case
+    var initialData = [['2019-02-14','201.89592700','25.30','',''],
+                       ['2019-03-13','104.50000000','20.25','',''],
+                       ['2019-03-13','5.55555600','1.00','',''],
+                       ['2019-03-13','5.55555600','1.00','',''],
+                       ['2019-03-13','5.55555600','1.00','',''],
+                       ['2019-03-13','38.88888900','7.00','',''],
+                       ['2019-03-30','3.55968800','1.00','',''],
+                       ['2019-03-30','3.56238300','1.00','',''],
+                       ['2019-03-30','3.56293500','1.00','',''],
+                       ['2019-03-30','24.93663400','6.98','',''],
+                       ['2019-04-09','14.25000000','4.14','',''],
+                       ['2019-05-09','14.25000000','4.22','',''],
+                       ['2019-06-10','19.00000000','6.19','',''],
+                       ['2019-09-08','7.60000000','1.34','',''],
+                       ['2019-10-09','49.40000000','10.18','',''],
+                       ['2019-11-08','25.65000000','6.20','',''],
+                       ['2019-12-07','43.46250000','8.40','',''],
+                       ['2020-01-07','4.50000000','0.88','',''],
+                       ['2020-02-01','61.91077800','13.76','',''],
+                       ['2020-02-09','23.51250000','6.24','',''],
+                       ['2020-02-09','20.35000000','5.40','',''],
+                       ['2020-03-06','22.05640000','5.23','',''],
+                       ['2020-03-09','75.76250000','14.54','',''],
+                       ['2020-04-06','24.21220000','3.73','',''],
+                       ['2020-04-08','25.65000000','4.23','',''],
+                       ['2020-05-04','','','829.14000000','151.26'],
+                       ['2020-05-06','16.37960000','','',''],	
+                       ['2020-05-09','26.60000000','','',''],	
+                       ['2020-06-05','6.30000000','','',''],	
+                       ['2020-06-10','37.78054500','','',''],	
+                       ['2020-07-07','5.09400000','','','']];
+
+    var TestRun = function (round) {
+      // mimic calculateFIFO_() 
+      if (validate(sheet)) {
+          
+        var lots = getLots(sheet);
+        var sales = getSales(sheet);
+
+        calculateFifo(sheet, lots, sales);
+        
+        // output the current date and time as the time last completed
+        var now = Utilities.formatDate(new Date(), 'CST', 'MMMM dd, yyyy HH:mm');
+        sheet.getRange('I1').setValue('Last calculation succeeded '+now);
+            
+      } else {
+        
+        var now = Utilities.formatDate(new Date(), 'CST', 'MMMM dd, yyyy HH:mm');
+        sheet.getRange('I1').setValue('Data validation failed '+now);
+      }
+
+      // check if test passed or failed
+      for (var j = 3; j < 28; j++) {
+        equal( sheet.getRange('F'+j).getValue(), "100% Sold", "Round "+round+" Test for Lot Sold In Full Later : Row "+j+" Status : expected 100% sold" );
+      }
+
+      equal( sheet.getRange('F28').getValue(), "Long-term", "Round "+round+" Test for Split into Long-Term Sale : Row 28 Status : expected long-term cost basis" );
+      equal( sheet.getRange('G28').getValue().toFixed(2), 69.67, "Round "+round+" Test for Split into Long-Term Sale : Row 28 Cost Basis : expected $69.67 cost basis" );
+      equal( sheet.getRange('H28').getValue().toFixed(2), 5.46, "Round "+round+" Test for Split into Long-Term Sale : Row 28 Gain(Loss) : expected $5.46 gain" );
+      // also check the NOTE content...
+      // A28: Originally 829.14000000 BAT was sold for $151.26 and split into rows 28 and 29.
+      // D28: Sold lots from row ??? on ????-??-?? to row 13 on 2019-04-09.
+
+      equal( sheet.getRange('F29').getValue(), "Short-term", "Round "+round+" Test for Split into Short-Term Sale : Row 29 Status : expected short-term cost basis" );
+      equal( sheet.getRange('G29').getValue().toFixed(2), 90.54, "Round "+round+" Test for Split into Short-Term Sale : Row 29 Cost Basis : expected $90.54 cost basis" );
+      equal( sheet.getRange('H29').getValue().toFixed(2), -14.41, "Round "+round+" Test for Split into Short-Term Sale : Row 29 Gain(Loss) : expected $(14.41) gain" );
+      // also check the NOTE content...
+      // A28: Originally 829.14000000 BAT was sold for $151.26 and split into rows 28 and 29.
+      // D29: Sold lots from row ??? on ????-??-?? to row 27 on 2020-04-08.
+    };
+    
+    for (var i = 0; i < initialData.length; i++) {
+      sheet.getRange('A'+(i+3)+':E'+(i+3)).setValues([initialData[i]]);
+    }
+    
+    // run the 31 assumption checks twice, as there are two code paths to test when a row split is involved
+    expect(62);
+    TestRun(1);
+    TestRun(2);
+
     // clean up temp sheet
     SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheet);
   });
