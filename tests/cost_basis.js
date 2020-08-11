c51ea581def5e8af176eb72e97bf1df7ed01f124
dogracer <d@jumphook.com>
2020-08-10 23:47:22 -0500

    Added remaining tests 1,2,3,5,7

--- tests/cost_basis.js
+++ tests/cost_basis.js
@@ -20,8 +20,13 @@
 
 QUnit.helpers( this );
 function testFunctions() {
+  test1_DataValidation();
+  test2_DataValidation();
+  test3_DataValidation();
   test4_CostBasis();
+  test5_CostBasis();
   test6_CostBasis();
+  test7_CostBasis();
   test8_CostBasis();  
   test9_CostBasis();
 }
@@ -38,10 +43,129 @@ function doGet( e ) {
 
 
 /** 
+ * test1 for function validate(sheet)
+ */
+function test1_DataValidation() {
+  QUnit.test( "test1 - Data Validation - Date Out of Order", function() {
+    // test data for this test case
+    var initialData = [['2017-01-01','1.0','1000','',''],
+                       ['2017-01-02','1.0','1000','',''],
+                       ['2017-01-02','','','0.5','2000'],
+                       ['2017-01-01','','','1.0','2000']];
+
+    // create temp sheet
+    var currentdate = new Date(); 
+    var uniqueSheetName = "TEST1(" + (currentdate.getMonth()+1) + "/"
+                + currentdate.getDate() + "/" 
+                + currentdate.getFullYear() + "@"  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds() + ")";
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(uniqueSheetName);
+    
+    var TestRun = function () {
+      var result = validate(sheet);    
+      equal(result, false, "Test for Date Out of Order Validation : Validation Error : expected validation to fail" );
+    };
+
+    // fill the in the test data
+    for (var i = 0; i < initialData.length; i++) {
+      sheet.getRange('A'+(i+3)+':E'+(i+3)).setValues([initialData[i]]);
+    }
+
+    // run the test
+    expect(1);
+    TestRun();
+    
+    // clean up temp sheet
+    SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheet);
+  });
+}
+
+/** 
+ * test2 for function validate(sheet)
+ */
+function test2_DataValidation() {
+  QUnit.test( "test2 - Data Validation - Coin Oversold", function() {
+    // test data for this test case
+    var initialData = [['2017-01-01','1.0','1000','',''],
+                       ['2017-01-02','1.0','1000','',''],
+                       ['2017-01-03','','','0.5','2000'],
+                       ['2017-01-04','','','2.0','2000']];
+
+    // create temp sheet
+    var currentdate = new Date(); 
+    var uniqueSheetName = "TEST2(" + (currentdate.getMonth()+1) + "/"
+                + currentdate.getDate() + "/" 
+                + currentdate.getFullYear() + "@"  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds() + ")";
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(uniqueSheetName);
+    
+    var TestRun = function () {
+      var result = validate(sheet);    
+      equal(result, false, "Test for Coin Oversold Condition : Validation Error : expected validation to fail" );
+    };
+
+    // fill the in the test data
+    for (var i = 0; i < initialData.length; i++) {
+      sheet.getRange('A'+(i+3)+':E'+(i+3)).setValues([initialData[i]]);
+    }
+
+    // run the test
+    expect(1);
+    TestRun();
+    
+    // clean up temp sheet
+    SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheet);
+  });
+}
+
+/** 
+ * test3 for function validate(sheet)
+ */
+function test3_DataValidation() {
+  QUnit.test( "test3 - Data Validation - Buy and Sell on Same Line", function() {
+    // test data for this test case
+    var initialData = [['2017-01-01','1.0','1000','',''],
+                       ['2017-01-02','1.0','1000','0.5',''],
+                       ['2017-01-03','','','0.5','2000']];
+
+    // create temp sheet
+    var currentdate = new Date(); 
+    var uniqueSheetName = "TEST3(" + (currentdate.getMonth()+1) + "/"
+                + currentdate.getDate() + "/" 
+                + currentdate.getFullYear() + "@"  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds() + ")";
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(uniqueSheetName);
+    
+    var TestRun = function () {
+      var result = validate(sheet);    
+      equal(result, false, "Test for Buy and Sell on Same Line : Validation Error : expected validation to fail" );
+    };
+
+    // fill the in the test data
+    for (var i = 0; i < initialData.length; i++) {
+      sheet.getRange('A'+(i+3)+':E'+(i+3)).setValues([initialData[i]]);
+    }
+
+    // run the test
+    expect(1);
+    TestRun();
+    
+    // clean up temp sheet
+    SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheet);
+  });
+}
+
+/** 
  * test4 for function calculateFifo(sheet, lots, sales)
  */
 function test4_CostBasis() {
-  QUnit.test( "test4 - Simple Partial Sale - Two Rounds", function() {
+  QUnit.test( "test4 - Simple Partial Short-Term Sale - Two Rounds", function() {
     // test data for this test case
     var initialData = [['2017-01-01','1.0','1000','',''],
                        ['2017-01-03','','','0.5','1000']];
@@ -99,6 +223,70 @@ function test4_CostBasis() {
     SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheet);
   });
 }
+
+/** 
+ * test5 for function calculateFifo(sheet, lots, sales)
+ */
+function test5_CostBasis() {
+  QUnit.test( "test5 - Simple Whole Long-Term Sale - Two Rounds", function() {
+    // test data for this test case
+    var initialData = [['2017-01-01','1.0','1000','',''],
+                       ['2018-01-02','','','1.0','2000']];
+
+    // create temp sheet
+    var currentdate = new Date(); 
+    var uniqueSheetName = "TEST5(" + (currentdate.getMonth()+1) + "/"
+                + currentdate.getDate() + "/" 
+                + currentdate.getFullYear() + "@"  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds() + ")";
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(uniqueSheetName);
+    
+    var TestRun = function (round) {
+      // mimic calculateFIFO_() 
+      if (validate(sheet)) {
+      
+        var lots = getLots(sheet);
+        var sales = getSales(sheet);
+      
+        calculateFifo(sheet, lots, sales);
+        
+        // output the current date and time as the time last completed
+        var now = Utilities.formatDate(new Date(), 'CST', 'MMMM dd, yyyy HH:mm');
+        sheet.getRange('I1').setValue('Last calculation succeeded '+now);
+            
+      } else {
+        
+        var now = Utilities.formatDate(new Date(), 'CST', 'MMMM dd, yyyy HH:mm');
+        sheet.getRange('I1').setValue('Data validation failed '+now);
+      }
+      
+      // check if test passed or failed
+      equal( sheet.getRange('F3').getValue(), "100% Sold", "Round "+round+" Test for Whole Long-Term Sale : Row 3 Status : expected all coin sold" );
+      equal( sheet.getRange('G3').getValue(), "", "Round "+round+" Test for Whole Long-Term Sale : Row 3 Cost Basis : expected no cost basis" );
+      equal( sheet.getRange('H3').getValue(), "", "Round "+round+" Test for Whole Long-Term Sale : Row 3 Gain(Loss) : expected no gain" );
+      equal( sheet.getRange('D4').getNote(), "Sold lots from row ??? on ????-??-?? to row 3 on 2017-01-01.", "Round "+round+" Test for Lot Sold Hint : Row 4 Sold : expected sold from row ? to 3" );
+      equal( sheet.getRange('F4').getValue(), "Long-term", "Round "+round+" Test for Whole Long-Term Sale : Row 4 Status : expected long-term cost basis" );
+      equal( sheet.getRange('G4').getValue().toFixed(2), 1000.00, "Round "+round+" Test for Whole Long-Term Sale : Row 4 Cost Basis : expected 1000 cost basis" );
+      equal( sheet.getRange('H4').getValue().toFixed(2), 1000.00, "Round "+round+" Test for Whole Long-Term Sale : Row 4 Gain(Loss) : expected 1000 gain" );
+    };
+
+    // fill the in the test data
+    for (var i = 0; i < initialData.length; i++) {
+      sheet.getRange('A'+(i+3)+':E'+(i+3)).setValues([initialData[i]]);
+    }
+
+    // run the 7 assumption checks twice, to make sure we get same result each time
+    expect(14);
+    TestRun(1);
+    TestRun(2);
+    
+    // clean up temp sheet
+    SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheet);
+  });
+}
+
 
 /** 
  * test6 for function calculateFifo(sheet, lots, sales)
@@ -171,6 +359,65 @@ function test6_CostBasis() {
     TestRun(1);
     TestRun(2);
 
+    // clean up temp sheet
+    SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheet);
+  });
+}
+
+
+/** 
+ * test7 for function calculateFifo(sheet, lots, sales)
+ */
+function test7_CostBasis() {
+  QUnit.test( "test7 - No Sale - Two Rounds", function() {
+    // test data for this test case
+    var initialData = [['2017-01-01','1.0','1000','','']];
+
+    // create temp sheet
+    var currentdate = new Date(); 
+    var uniqueSheetName = "TEST7(" + (currentdate.getMonth()+1) + "/"
+                + currentdate.getDate() + "/" 
+                + currentdate.getFullYear() + "@"  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds() + ")";
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(uniqueSheetName);
+    
+    var TestRun = function (round) {
+      // mimic calculateFIFO_() 
+      if (validate(sheet)) {
+      
+        var lots = getLots(sheet);
+        var sales = getSales(sheet);
+      
+        calculateFifo(sheet, lots, sales);
+        
+        // output the current date and time as the time last completed
+        var now = Utilities.formatDate(new Date(), 'CST', 'MMMM dd, yyyy HH:mm');
+        sheet.getRange('I1').setValue('Last calculation succeeded '+now);
+            
+      } else {
+        
+        var now = Utilities.formatDate(new Date(), 'CST', 'MMMM dd, yyyy HH:mm');
+        sheet.getRange('I1').setValue('Data validation failed '+now);
+      }
+      
+      // check if test passed or failed
+      equal( sheet.getRange('F3').getValue(), "0% Sold", "Round "+round+" Test for No Sale : Row 3 Status : expected no coin sold" );
+      equal( sheet.getRange('G3').getValue(), "", "Round "+round+" Test for No Sale : Row 3 Cost Basis : expected no cost basis" );
+      equal( sheet.getRange('H3').getValue(), "", "Round "+round+" Test for No Sale : Row 3 Gain(Loss) : expected no gain" );
+    };
+
+    // fill the in the test data
+    for (var i = 0; i < initialData.length; i++) {
+      sheet.getRange('A'+(i+3)+':E'+(i+3)).setValues([initialData[i]]);
+    }
+
+    // run the 3 assumption checks twice, to make sure we get same result each time
+    expect(6);
+    TestRun(1);
+    TestRun(2);
+    
     // clean up temp sheet
     SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheet);
   });
