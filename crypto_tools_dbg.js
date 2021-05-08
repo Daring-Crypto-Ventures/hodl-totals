b6a62dc1c3f3e7ea6f160bf4416589aa58784e11
dogracer <dogracer@tutanota.com>
2021-05-08 10:30:48 -0500

    Prep core algorithm for standalone usage

--- crypto_tools_dbg.js
+++ crypto_tools_dbg.js
@@ -4,21 +4,22 @@
  */
 function runTests() {
   // original example data
-  var initialData = [
-    ['2017/01/01','0.20000000','2000.00',            ,         , , , ,'Enter coin buys in the left-hand columns. Include fees in the cost.'],
-    ['2018/02/01','0.60000000','6000.00',            ,         , , , ,'Enter everything in chronological order.'],
-    ['2018/02/01',            ,         ,'0.05000000','1000.00', , , ,'Enter coin sales in the right-hand columns, again, including fees.'],
-    ['2018/03/01',            ,         ,'0.05000000','1000.00', , , ,'The status column provides useful information for each transaction.'],
-    ['2018/03/01',            ,         ,'0.30000000','6000.00', , , ,'If a sale includes short and long-term components, it is split.'], 
+  var initialDataTest0 = [
+    ['2017/01/01','0.20000000','2000.00',            ,         , , , ,''],
+    ['2018/02/01','0.60000000','6000.00',            ,         , , , ,''],
+    ['2018/02/01',            ,         ,'0.05000000','1000.00', , , ,''],
+    ['2018/03/01',            ,         ,'0.05000000','1000.00', , , ,''],
+    ['2018/03/01',            ,         ,'0.30000000','6000.00', , , ,''], 
     ['2018/03/02','0.40000000','4000.00',            ,         , , , ,''],
-    ['2018/03/03','0.80000000','8000.00',            ,         , , , ,'If you would like to sort or filter to analyze your results, it is'],
-    ['2018/03/04','0.60000000','6000.00',            ,         , , , ,'recommended that you copy the results to a blank spreadsheet.'],
+    ['2018/03/03','0.80000000','8000.00',            ,         , , , ,''],
+    ['2018/03/04','0.60000000','6000.00',            ,         , , , ,''],
     ['2018/03/05',            ,         ,'0.10000000', '500.00', , , ,''],
-    ['2018/03/06',            ,         ,'0.10000000','1000.00', , , ,'Create a copy of the blank spreadsheet for each coin you trade'],
-    ['2018/03/07',            ,         ,'0.10000000','2000.00', , , ,'The notes column is a great place to keep track of fees,']];
+    ['2018/03/06',            ,         ,'0.10000000','1000.00', , , ,''],
+    ['2018/03/07',            ,         ,'0.10000000','2000.00', , , ,''],
+    ['', , , , , , , , , ]];
     
   // bug p0 data
-  var initialData = [
+  var initialDataTest1 = [
     ['2019-02-14','201.89592700','25.30',         ,        , , , ,''],
     ['2019-03-13','104.50000000','20.25',         ,        , , , ,''],
     ['2019-03-13',  '5.55555600', '1.00',         ,        , , , ,''],
@@ -32,27 +33,29 @@ function runTests() {
     ['2019-04-09', '14.25000000', '4.14',         ,        , , , ,''],
     ['2019-05-09', '14.25000000', '4.22',         ,        , , , ,''],
     ['2019-06-10', '19.00000000', '6.19',         ,        , , , ,''],
-    ['2019-09-08',  '7.60000000', '1.34',         ,        , , , ,'Expecting 100% Sold'],
-    ['2019-10-09', '49.40000000','10.18',         ,        , , , ,'Expecting 100% Sold'],
-    ['2019-11-08', '25.65000000', '6.20',         ,        , , , ,'Expecting 100% Sold'],
-    ['2019-12-07', '43.46250000', '8.40',         ,        , , , ,'Expecting 100% Sold'],
-    ['2020-01-07',  '4.50000000', '0.88',         ,        , , , ,'Expecting 100% Sold'],
-    ['2020-02-01', '61.91077800','13.76',         ,        , , , ,'Expecting 100% Sold'],
-    ['2020-02-09', '23.51250000', '6.24',         ,        , , , ,'Expecting 100% Sold'],
-    ['2020-02-09', '20.35000000', '5.40',         ,        , , , ,'Expecting 100% Sold'],
-    ['2020-03-06', '22.05640000', '5.23',         ,        , , , ,'Expecting 100% Sold'],
-    ['2020-03-09', '75.76250000','14.54',         ,        , , , ,'Expecting 100% Sold'],
-    ['2020-04-06', '24.21220000', '3.73',         ,        , , , ,'Expecting 100% Sold'],
-    ['2020-04-08',   '25.650000', '4.23',         ,        , , , ,'Expecting 100% Sold'],
-    ['2020-05-04',              ,       , '829.14','151.26', , , ,'Expecting Split-Into-Short-Term Cost Basis to be $90.54']];
+    ['2019-09-08',  '7.60000000', '1.34',         ,        , , , ,''],
+    ['2019-10-09', '49.40000000','10.18',         ,        , , , ,''],
+    ['2019-11-08', '25.65000000', '6.20',         ,        , , , ,''],
+    ['2019-12-07', '43.46250000', '8.40',         ,        , , , ,''],
+    ['2020-01-07',  '4.50000000', '0.88',         ,        , , , ,''],
+    ['2020-02-01', '61.91077800','13.76',         ,        , , , ,''],
+    ['2020-02-09', '23.51250000', '6.24',         ,        , , , ,''],
+    ['2020-02-09', '20.35000000', '5.40',         ,        , , , ,''],
+    ['2020-03-06', '22.05640000', '5.23',         ,        , , , ,''],
+    ['2020-03-09', '75.76250000','14.54',         ,        , , , ,''],
+    ['2020-04-06', '24.21220000', '3.73',         ,        , , , ,''],
+    ['2020-04-08',   '25.650000', '4.23',         ,        , , , ,''],
+    ['2020-05-04',              ,       , '829.14','151.26', , , ,''],
+    ['', , , , , , , , , ]];
   
   
   // execute the tests based on the test dataset
-  var result = test_FIFOCalc(initialData);
+  var result0 = FIFOCalc(initialDataTest0);
+  Logger.log('FIFOCalc() Test0 result '+ result0);
+  var result1 = FIFOCalc(initialDataTest1);
+  Logger.log('FIFOCalc() Test1 result '+ result1);
   
-  Logger.log('testFIFOCalc() test result '+ result);
-  
-  return result;
+  return result0 || result1;
 }
 
 /**
@@ -63,7 +66,7 @@ function runTests() {
  *
  * @return Date object corresponding to that string input.
  */
-function test_dateFromString(dateStr, incYear) {
+function dateFromString(dateStr, incYear) {
     
   var year = Number(+dateStr.substring(0, 4));
   var month = Number(+dateStr.substring(5, 7));
@@ -73,11 +76,39 @@ function test_dateFromString(dateStr, incYear) {
 }
 
 /**
+ * Extract non-empty rows of either coin purchase data or sale data from the sheet.
+ * 
+ * @param sheet the google sheet with the crypto data
+ *
+ * @return lots 2D array of {date, amt coin, price}
+ */
+ function getOrderList(dateDisplayValues, lastRow, coinAndPriceData) {
+  var orderList;
+  var order;
+  orderList = new Array();
+  order = 0;
+  
+  // compact the data into a contiguous array
+  for (var row = 2; row < lastRow; row++) {   
+    if (coinAndPriceData[row][0] > 0) {
+      orderList[order] = new Array(4);
+      orderList[order][0] = dateDisplayValues[row][0]; // date of order
+      orderList[order][1] = coinAndPriceData[row][0];  // amount of coin bought or sold
+      orderList[order][2] = coinAndPriceData[row][1];  // purchase price or sale price
+      orderList[order][3] = row+1;
+      order++;
+    }
+  }
+
+  return orderList;
+}
+
+/**
  * Test the FIFO Calculation function outside of the spreadsheet context
  *
  * @return true = passm, false = fail .
  */
-function test_FIFOCalc(data) {
+function FIFOCalc(data) {
   var dateArray;
   var lotsArray;
   var salesArray;
@@ -105,7 +136,7 @@ function test_FIFOCalc(data) {
   var sales = getOrderList(dateArray, data.length, salesArray);
   Logger.log('Detected ' + sales.length + ' sales of TESTCOIN.');
     
-  test_calculateFIFO(data, lots, sales);
+  calculateFIFO(data, lots, sales);
     
   // TODO - check calculated columns in data to see if they matched expected
   // if didn't match, return false
@@ -123,7 +154,7 @@ function test_FIFOCalc(data) {
  * 
  * @param sheet the google sheet with the crypto data
  */
-function test_calculateFIFO(data, lots, sales) {
+function calculateFIFO(data, lots, sales) {
   var shift; // Integer
   var lotCount; // Integer
   var lotCoinRemain; // Double
@@ -134,7 +165,8 @@ function test_calculateFIFO(data, lots, sales) {
   var sellCoin; // Double
   var sellRecd; // Double
   var sellRow; // Integer
-  const MILLIS_PER_DAY = 1000 * 60 * 60 * 24; // not needed?
+  const MILLIS_PER_DAY = 1000 * 60 * 60 * 24;
+  const ONE_SATOSHI = .00000001;
 
   shift = 0;
   lotCount = 0;
@@ -142,12 +174,20 @@ function test_calculateFIFO(data, lots, sales) {
   // start with num coins that were necessarily bought in "lot 0'
   lotCoinRemain = lots[0][1];
 
+  // if no sales yet, mark the status of the first lot as 0% sold
+  if (sales.length === 0) {
+    data[0][5] = '0% Sold';
+  }
+
   for (var sale = 0; sale < sales.length; sale++) {
     var termSplit; // Boolean
+    var prevSplitRow; // Boolean
     var splitFactor; // Double
     var totalCoin; // Double
     var totalCost; // Double
+    var startingLotCount;
     termSplit = false; // flag if sale involved both short-term and long-term holdings
+    prevSplitRow = false; // flag to avoid creating extra rows when running calc repeatedly on same sheet
     splitFactor = 0; // ratio of totalCoin to sellCoin
     totalCoin = 0; // running total of coins for basis
     totalCost = 0; // running total of dollar cost for basis
@@ -155,6 +195,7 @@ function test_calculateFIFO(data, lots, sales) {
     sellCoinRemain = sellCoin = sales[sale][1];
     sellRecd = sales[sale][2];
     sellRow = sales[sale][3];
+    startingLotCount = lotCount;
 
     for (var lot = lotCount; lot < lots.length; lot++) {
       var thisTerm; // Date
@@ -162,11 +203,9 @@ function test_calculateFIFO(data, lots, sales) {
       var originalDate; // Date
       var originalCoin; // Double
       var originalCost; // Double
-      var lotDate; // Date
       var lotCoin; // Double
       var lotCost; // Double
       var lotRow; // Integer
-      lotDate = dateFromString(lots[lot][0],0);
       lotCoin = lots[lot][1];
       lotCost = lots[lot][2];
       lotRow = lots[lot][3];
@@ -178,12 +217,12 @@ function test_calculateFIFO(data, lots, sales) {
       // calculate and post the cost basis and the gain or loss
       if (sellCoinRemain <= lotCoinRemain) {
         
-        if (Math.abs(sellCoinRemain - lotCoinRemain) <= .00000001) {
+        if (Math.abs(sellCoinRemain - lotCoinRemain) <= ONE_SATOSHI) {
           // all of this lot was sold
           data[lotRow][5] = '100% Sold';
 
           // if there are more lots to process, advance the lotCount before breaking out
-          if (lotCount < lots.length) {
+          if ((lotCount+1) < lots.length) {
             lotCount++;  
             lotCoinRemain = lots[lotCount][1];
           }
@@ -199,20 +238,31 @@ function test_calculateFIFO(data, lots, sales) {
 
         // if sale more than 1 year and 1 day from purchase date mark as long-term gains        
         if (!termSplit) {
-          if ((sellDate.getTime() - thisTerm.getTime()) / MILLIS_PER_DAY >= 0) {
+          if ((sellDate.getTime() - thisTerm.getTime()) / MILLIS_PER_DAY > 0) {
             data[sellRow+shift][5] = 'Long-term';
           } else {
             data[sellRow+shift][5] ='Short-term';
           }
         }
 
+        if (!prevSplitRow) {
         // calculate and post results 
         totalCoin = totalCoin + sellCoinRemain;
         totalCost = totalCost + (lotCost * (sellCoinRemain / lotCoin));
         costBasis = sellCoin * (totalCost / totalCoin) * (1 - splitFactor);
         gainLoss = (sellRecd * (1 - splitFactor)) - costBasis;       
+
         data[sellRow+shift][6] = costBasis;
         data[sellRow+shift][7] = gainLoss;
+
+          // take note note of which lots were sold and when
+          if (startingLotCount === lot) {
+            data[sellRow+shift][8] = 'Sold lot from row '+lots[lot][3]+' on '+lots[lot][0]+'.';
+          } else {
+            data[sellRow+shift][8] = 'Sold lots from row '+
+              lots[startingLotCount][3]+' on '+lots[startingLotCount][0]+' to row '+lots[lot][3]+' on '+lots[lot][0]+'.';  
+          }
+        }
         
         break; // Exit the inner for loop
       }
@@ -226,20 +276,18 @@ function test_calculateFIFO(data, lots, sales) {
           nextTerm = sellDate; //no look-ahead date, so no term-split, fall thru the next case
         }
         
-        // look ahead for a term split, and if a split exists,
-        // set the split factor (% to allocate to either side of the split),
-        // and calculate and post the first half of the split
-        if (((sellDate.getTime() - thisTerm.getTime()) >= 0) && ((sellDate.getTime() - nextTerm.getTime()) < 0)) {
+        // look ahead for a term split, do additional calculations, and
+        // split both sides of the split on two different rows
+        if (((sellDate.getTime() - thisTerm.getTime()) / MILLIS_PER_DAY > 0) 
+          && ((sellDate.getTime() - nextTerm.getTime()) / MILLIS_PER_DAY < 0)) {
          
           termSplit = true;
 
+          // calculate the split factor
           totalCoin = totalCoin + lotCoinRemain;
           totalCost = totalCost + (lotCost * (lotCoinRemain / lotCoin));
-
-          // calculate the split factor
           splitFactor = totalCoin / sellCoin;
 
-          // post the long-term split and continue
           costBasis = sellCoin * (totalCost / totalCoin) * splitFactor; // average price
           gainLoss = (sellRecd * splitFactor) - costBasis;
 
@@ -247,29 +295,52 @@ function test_calculateFIFO(data, lots, sales) {
           originalCoin = Number(data[sellRow+shift][3]);
           originalCost = Number(data[sellRow+shift][4]);
           
-          data[sellRow+shift][6] = costBasis;
-          data[sellRow+shift][7] = gainLoss;
-          
-          Logger.log('Split into (rows '+(sellRow+shift)+
-              ' and '+(sellRow+shift+1)+'). Amount of coin sold was '+originalCoin.toFixed(8)+
-              ', and original amount was $'+originalCost.toFixed(2)+'.');
-          
+          // post the long-term split       
           data[sellRow+shift][3] = originalCoin * splitFactor;
           data[sellRow+shift][4] = originalCost * splitFactor;
           data[sellRow+shift][5] = 'Long-term';
+          data[sellRow+shift][6] = costBasis;
+          data[sellRow+shift][7] = gainLoss;
           
-          // create the new row to handle second part of the term split
-          data.splice(sellRow+shift, 0, new Array(9));
-          shift++;
-          
-          Logger.log('A'+(sellRow+shift)+
-             'Sale split into (rows '+(sellRow+shift-1)+' and '+(sellRow+shift)+
-             '). Original amount of coin sold was '+originalCoin.toFixed(8)+
-             ', and original amount was $'+originalCost.toFixed(2)+'.');
-          
-          data[sellRow+shift][3] = originalCoin * (1 - splitFactor);
-          data[sellRow+shift][4] = originalCost * (1 - splitFactor);
-          data[sellRow+shift][5] = 'Short-term';
+          // take note note of which lots were sold and when
+          if (startingLotCount === lot) {
+            data[sellRow+shift][8] = 'Sold lot from row '+lots[lot][3]+' on '+lots[lot][0]+'.';
+          } else {
+            data[sellRow+shift][8] = 'Sold lots from row '+
+              lots[startingLotCount][3]+' on '+lots[startingLotCount][0]+' to row '+lots[lot][3]+' on '+lots[lot][0]+'.';  
+          }
+
+          // Don't create note/new row if there is negligable value left in the short-term part
+          // likely caused by rounding errors repeating the cost basis calc on the same sheet
+          if (originalCoin * (1 - splitFactor) >= ONE_SATOSHI) {
+            
+            var splitNoteText = 'Originally '+originalCoin.toFixed(8)+' '+
+            sheet.getName().replace(/ *\([^)]*\) */g, "")+' was sold for $'+originalCost.toFixed(2)+
+               ' and split into rows '+(sellRow+shift)+' and '+(sellRow+shift+1)+'.';
+            Logger.log("Row "+(sellRow+shift)+": "+splitNoteText);
+            // create the new row for the short-term part of the term split
+            data.splice(sellRow+shift, 0, new Array(9));
+		    // shift to the next row to post the short-term split
+            shift++;
+            Logger.log("Row "+(sellRow+shift)+": "+splitNoteText);
+            data[sellRow+shift][3] = originalCoin * (1 - splitFactor);
+            data[sellRow+shift][4] = originalCost * (1 - splitFactor);
+            data[sellRow+shift][5] = 'Short-term';
+
+            // update lots after the split transaction to account for the inserted row
+            for (var lotAfterSplit = 0; lotAfterSplit < lots.length; lotAfterSplit++) {
+              if (lots[lotAfterSplit][3] >= (sellRow+shift)) {
+                lots[lotAfterSplit][3]++;
+              }
+            }
+
+            // reset the startingLot to point at the lot after the lot sold via long-term split
+            // so that short-term part of split will get an accurate note attached
+            startingLotCount = lot+1;
+          }
+          else {
+            prevSplitRow = true;
+          }
           
           totalCoin = 0;
           totalCost = 0;       
@@ -284,7 +355,6 @@ function test_calculateFIFO(data, lots, sales) {
         // subtract the lot amount from the remaining coin to be sold,
         // and set up variables for the next lot, since this lot is completely used up
         sellCoinRemain = sellCoinRemain - lotCoinRemain;
-        
         data[lotRow][5] = '100% Sold';
         lotCount++;
         if (lotCount < lots.length) {
