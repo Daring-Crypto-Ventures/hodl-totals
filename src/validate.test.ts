7a1924ebbb52dda771a007e9835eb614f92652e4
dogracer <dogracer@tutanota.com>
2021-05-19 23:53:12 -0500

    Add test2,test3 for validate to Local run unit test

--- src/validate.test.ts
+++ src/validate.test.ts
@@ -1,8 +1,7 @@
 import validate from './validate';
 
 /**
- * jest unit tests
- * https://medium.com/@wesvdl1995/testing-nodejs-code-with-jest-28267a69324
+ *  unit test1
  *
  */
 test('Cost Basis test1 - Data Validation - Date Out of Order', () => {
@@ -15,6 +14,51 @@ test('Cost Basis test1 - Data Validation - Date Out of Order', () => {
             ['2017-01-02', 1.0, 1000, 0, 0],
             ['2017-01-02', 0, 0, 0.5, 2000],
             ['2017-01-01', 0, 0, 1.0, 2000]];
+
+        const TestRun = function (): void {
+            const result = validate(initialData);
+            expect(result).toBeFalsy();
+        };
+        TestRun();
+    }
+});
+
+/**
+ *  unit test2
+ *
+ */
+test('Cost Basis test2 - Data Validation - Coin Oversold', () => {
+    if (typeof ScriptApp === 'undefined') {
+        // test data for this test case
+        const initialData: [string, number, number, number, number][] = [
+            ['', 0, 0, 0, 0],
+            ['', 0, 0, 0, 0],
+            ['2017-01-01', 1.0, 1000, 0, 0],
+            ['2017-01-02', 1.0, 1000, 0, 0],
+            ['2017-01-03', 0, 0, 0.5, 2000],
+            ['2017-01-04', 0, 0, 2.0, 2000]];
+
+        const TestRun = function (): void {
+            const result = validate(initialData);
+            expect(result).toBeFalsy();
+        };
+        TestRun();
+    }
+});
+
+/**
+ *  unit test3
+ *
+ */
+test('Cost Basis test3 - Data Validation - Buy and Sell on Same Line', () => {
+    if (typeof ScriptApp === 'undefined') {
+        // test data for this test case
+        const initialData: [string, number, number, number, number][] = [
+            ['', 0, 0, 0, 0],
+            ['', 0, 0, 0, 0],
+            ['2017-01-01', 1.0, 1000, 0, 0],
+            ['2017-01-02', 1.0, 1000, 0.5, 0],
+            ['2017-01-03', 0, 0, 0.5, 2000]];
 
         const TestRun = function (): void {
             const result = validate(initialData);
