f5dcc7ebd8ba8dea9a6b860f045e2cca9ae64d0a
dogracer <dogracer@tutanota.com>
2021-05-31 11:01:27 -0500

    To fix Mac build, using rollup.js instead of custom file output renames

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,7 @@
-import inquirer from 'inquirer';
-
 // The standalone app functionality goes here
 function displayData(): void {
     console.log('--> TODO HODL Totals data table <--');
 }
 
-enum Commands {
-    LoadCostBasisExample = 'Load Example',
-    Quit = 'Quit'
-}
-
-function promptUser(): void {
-    displayData();
-    inquirer.prompt({
-        type: 'list',
-        name: 'command',
-        message: 'Choose option',
-        choices: Object.values(Commands)
-    }).then(answers => {
-        switch (answers.command) {
-            case Commands.LoadCostBasisExample:
-                // TODO implmement this
-                console.log('Example not accessible from node.js place yet.');
-                promptUser();
-                break;
-            default:
-        }
-    });
-}
-
-console.clear();
-promptUser();
+displayData();
+console.log('Example not accessible from node.js place yet.');
