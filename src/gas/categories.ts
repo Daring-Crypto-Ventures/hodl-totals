adee67f376398773c085847e9123cf64aa8824b0
dogracer <dogracer@tutanota.com>
2021-06-05 17:48:31 -0500

    Add write-ups for WHY taxable vs not on the Categories sheet

--- src/gas/categories.ts
+++ src/gas/categories.ts
@@ -12,8 +12,8 @@
  */
 export default function newCategorySheet(): GoogleAppsScript.Spreadsheet.Sheet | null {
     // Initial set of categories provided out of the box
-    const header = ['Categories', 'Type', 'Tax Status', 'Inflow Categories', 'Outflow Categories'];
-    const initialData = [
+    const header = ['Categories', 'Type', 'Tax Status', 'Inflow Categories', 'Outflow Categories', 'Tax Status Justification'];
+    const data = [
         ['USD Deposit', 'Cash In', 'Not Taxable', '=IF(EXACT(B2,"Inflow"), A2, "")', '=IF(AND(EXACT(B2,"Outflow"),NOT(EXACT(A2,"Traded")),NOT(EXACT(A2,"Tx Fee"))), A2, "")'],
         ['USD Withdrawal', 'Cash Out', 'Taxable', '=IF(EXACT(B3,"Inflow"), A3, "")', '=IF(AND(EXACT(B3,"Outflow"),NOT(EXACT(A3,"Traded")),NOT(EXACT(A3,"Tx Fee"))), A3, "")'],
         ['Active Airdrop', 'Inflow', 'Taxable', '=IF(EXACT(B4,"Inflow"), A4, "")', '=IF(AND(EXACT(B4,"Outflow"),NOT(EXACT(A4,"Traded")),NOT(EXACT(A4,"Tx Fee"))), A4, "")'],
@@ -38,19 +38,56 @@ export default function newCategorySheet(): GoogleAppsScript.Spreadsheet.Sheet |
         ['Tx Fee', 'Outflow', 'Taxable', '=IF(EXACT(B23,"Inflow"), A23, "")', '=IF(AND(EXACT(B23,"Outflow"),NOT(EXACT(A23,"Traded")),NOT(EXACT(A23,"Tx Fee"))), A23, "")'],
         ['Unknown Outflow', 'Outflow', 'Taxable', '=IF(EXACT(B24,"Inflow"), A24, "")', '=IF(AND(EXACT(B24,"Outflow"),NOT(EXACT(A24,"Traded")),NOT(EXACT(A24,"Tx Fee"))), A24, "")']
     ];
+    const justificationLinks = [
+        ['IRS 1040 Instructions: ...A [taxable] transaction does not include the holding of virtual currency in a wallet or account', 'https://www.irs.gov/instructions/i1040gi'],
+        ['IRS 1040 Instructions: ...A [taxable] transaction involving virtual currency includes...A sale of virtual currency', 'https://www.irs.gov/instructions/i1040gi'],
+        ['IRS Memo 202035011: ...virtual currency in exchange for performing a microtask/service... the virtual currency received is taxable', 'https://www.irs.gov/pub/irs-wd/202035011.pdf'],
+        ['GRAY AREA: Nothing was actively done to receive the coins, nor was the airdrop a proportional reward to some other tokens already held', ''],
+        ['IRS FAQ Q9. Do I have income if I provide someone with a service and that person pays me with virtual currency?', 'https://www.irs.gov/individuals/international-taxpayers/frequently-asked-questions-on-virtual-currency-transactions#collapseCollapsible1622820578836'],
+        ['IRS FAQ Q24. How do I calculate my income from cryptocurrency I received following a hard fork?', 'https://www.irs.gov/individuals/international-taxpayers/frequently-asked-questions-on-virtual-currency-transactions#collapseCollapsible1622820578458'],
+        ['IRS FAQ Q31. I received virtual currency as a bona fide gift. Do I have income? (below $15,000/yr)', 'https://www.irs.gov/individuals/international-taxpayers/frequently-asked-questions-on-virtual-currency-transactions#collapseCollapsible1622820578306'],
+        ['IRS Pub 550: In general, any interest that you receive or that is credited to your account and can be withdrawn is taxable income.', 'https://www.irs.gov/taxtopics/tc403'],
+        ['IRS Notice 2014-21: Q-8. Does a taxpayer who “mines” virtual currency realize gross income upon receipt of the virtual currency resulting from those activities?', 'https://www.irs.gov/irb/2014-16_IRB#NOT-2014-21'],
+        ['IRS Pub 525 Other Income... Prizes or awards in goods or services must be included in your income at their FMV', 'https://www.irs.gov/publications/p525#en_US_2020_publink1000229578'],
+        ['IRS Pub 550: Gift for opening an account: If you receive noncash gifts or services for making deposits/opening an account...you may have to report it', 'https://www.irs.gov/publications/p550#en_US_2020_publink10009869'],
+        ['IRS FAQ Q17. How do I calculate my gain or loss when I exchange my virtual currency for other property?', 'https://www.irs.gov/individuals/international-taxpayers/frequently-asked-questions-on-virtual-currency-transactions#collapseCollapsible1622820578638'],
+        ['IRS NOTICE 2014-21: Q-8. Does a taxpayer who “mines” virtual currency realize gross income upon receipt of the virtual currency resulting from those activities?', 'https://www.irs.gov/irb/2014-16_IRB#NOT-2014-21'],
+        ['GRAY AREA: If not received for a service rendered, nothing was actively done to receive the coins like passive airdrop ', ''],
+        ['IRS Pub 525: Undocumentable Income spans from Bribes to Illegal Activity to Stolen Property: all taxable', 'https://www.irs.gov/publications/p525#en_US_2020_publink1000229492'],
+        ['GRAY AREA: If you can no longer control the coin, perhaps can consider it a loss equal to FMV of what was lost/stolen', ''],
+        ['IRS FAQ Q34. If I donate virtual currency to a charity, will I have to recognize income, gain, or loss?', 'https://www.irs.gov/individuals/international-taxpayers/frequently-asked-questions-on-virtual-currency-transactions#collapseCollapsible1622820578248'],
+        ['GRAY AREA: If you can no longer use the coin because the blockchain no longer advances, probably safe to consider it a loss equal to FMV at last transactable date', ''],
+        ['IRS FAQ Q16. Will I recognize a gain or loss if I exchange my virtual currency for other property?', 'https://www.irs.gov/individuals/international-taxpayers/frequently-asked-questions-on-virtual-currency-transactions#collapseCollapsible1622820578673'],
+        ['IRS FAQ Q14. Will I recognize a gain or loss if I pay someone with virtual currency for providing me with a service?', 'https://www.irs.gov/individuals/international-taxpayers/frequently-asked-questions-on-virtual-currency-transactions#collapseCollapsible1622820578720'],
+        ['IRS 1040 Instructions: ...A [taxable] transaction involving virtual currency includes...An exchange of virtual currency for other property, including for another virtual currency', 'https://www.irs.gov/instructions/i1040gi'],
+        ['IRS 1040 Instructions: ...A [taxable] transaction involving virtual currency includes...A disposition of a financial interest in virtual currency', 'https://www.irs.gov/instructions/i1040gi'],
+        ['IRS Fact Sheet on Misc Income', 'https://www.irs.gov/pub/irs-news/fs-07-26.pdf'],
+    ];
 
     if (typeof ScriptApp !== 'undefined') {
         const sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet('Categories');
 
         // populate the header cells
-        sheet.getRange('A1:E1').setValues([header]).setFontWeight('bold');
+        sheet.getRange('A1:F1').setValues([header]).setFontWeight('bold');
 
-        for (let i = 0; i < initialData.length; i++) {
-            sheet.getRange(`A${i + 2}:E${i + 2}`).setValues([initialData[i]]);
+        // fill in the raw data
+        for (let i = 0; i < data.length; i++) {
+            sheet.getRange(`A${i + 2}:E${i + 2}`).setValues([data[i]]);
+        }
+
+        // add text with URL links to colE data
+        for (let j = 0; j < data.length; j++) {
+            const range = SpreadsheetApp.getActive().getRange(`F${j + 2}`);
+            const richValue = SpreadsheetApp.newRichTextValue()
+                .setText(justificationLinks[j][0])
+                // @ts-expect-error Cannot find name setLinkUrl as no type declarations exists for this function, name is present when loaded in GAS
+                .setLinkUrl((justificationLinks[j][1] === '') ? null : justificationLinks[j][1])
+                .build();
+            range.setRichTextValue(richValue);
         }
 
         // autosize the 5 columns' widths to fit content
-        sheet.autoResizeColumns(1, 5);
+        sheet.autoResizeColumns(1, 6);
 
         // draw border around the rows that will fed into dropdowns in other sheets
         sheet.getRange('A2:C35').setBorder(true, true, true, true, false, false);
