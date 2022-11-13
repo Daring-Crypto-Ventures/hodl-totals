893199cace66a19b7dfac0da7ee9a66d125b4ece
dogracer <dogracer@tutanota.com>
2022-11-13 13:33:18 -0600

    Add formatting to the totals sheet

--- src/gas/totals.ts
+++ src/gas/totals.ts
@@ -48,15 +48,81 @@ export default function resetTotalSheet(): GoogleAppsScript.Spreadsheet.Sheet | 
                 sheet.appendRow(data);
             }
         }
+        
+        // format all populated coin rows
+        sheet.getRange(`D2:D${rowCount}`).setNumberFormat('yyyy-mm-dd');
+        sheet.getRange(`F2:F${rowCount}`).setNumberFormat('yyyy-mm-dd h:mm:ss').setHorizontalAlignment('right');
 
         // create filter around all populated coin rows
         sheet.getRange(`A1:G${rowCount}`).createFilter();
 
-        // autosize the columns' widths to fit content
+        // autosize the columns' widths, add conditional formatting
         sheet.autoResizeColumns(1, 7);
+        setConditionalFormattingRules(sheet, rowCount);
         SpreadsheetApp.flush();
         
         return sheet;
     }
     return null;
+}
+
+function setConditionalFormattingRules(sheet: GoogleAppsScript.Spreadsheet.Sheet, rowCount : number): void {
+    // Color the cell that displays last reconciliation date, off by and calc status
+    // to help users see if their sheet totals overall are in a healthy state
+    const lastRecRange = sheet.getRange(`D2:D${rowCount}`);
+    const offByRange = sheet.getRange(`E2:E${rowCount}`);
+    // and Color the success/failure cell to indicate health of the last calculation
+    const calcStatusRange = sheet.getRange(`G2:G${rowCount}`);
+
+    // extract the conditional rules set on all other cells on this sheet
+    var rules = SpreadsheetApp.getActiveSheet().getConditionalFormatRules();
+    var newRules = new Array() as [GoogleAppsScript.Spreadsheet.ConditionalFormatRule];
+    for (var i = 0; i < rules.length; i++) {
+        const ruleRange = rules[i].getRanges()?.[0].getA1Notation();
+        if ((ruleRange !== lastRecRange.getA1Notation()) && (ruleRange !== offByRange.getA1Notation()) && (ruleRange !== calcStatusRange.getA1Notation())) {
+            newRules.push(rules[i]);
+        }
+    }
+    // add back the rules for the cells we are formatting
+    newRules.push(SpreadsheetApp.newConditionalFormatRule()
+        .whenFormulaSatisfied("=D2>39816") // date recorded takes place after the bitcoin genesis block
+        .setBackground("#B7E1CD")  // green success
+        .setRanges([lastRecRange])
+        .build());
+    newRules.push(SpreadsheetApp.newConditionalFormatRule()
+        .whenFormulaSatisfied("=1")
+        .setBackground("#F4C7C3")  // red failure
+        .setRanges([lastRecRange])
+        .build());
+    newRules.push(SpreadsheetApp.newConditionalFormatRule()
+        .whenNumberBetween(-0.001, 0.001)
+        .setBackground("#B7E1CD")  // green success
+        .setRanges([offByRange])
+        .build());
+    newRules.push(SpreadsheetApp.newConditionalFormatRule()
+        .whenNumberNotBetween(-0.001, 0.001)
+        .setBackground("#FFFF00")  // yellow success
+        .setRanges([offByRange])
+        .build());
+    newRules.push(SpreadsheetApp.newConditionalFormatRule()
+        .whenFormulaSatisfied("=1")
+        .setBackground("#F4C7C3")  // red failure
+        .setRanges([offByRange])
+        .build());
+    newRules.push(SpreadsheetApp.newConditionalFormatRule()
+        .whenTextStartsWith("Succeeded")
+        .setBackground("#B7E1CD")  // green success
+        .setRanges([calcStatusRange])
+        .build());
+    newRules.push(SpreadsheetApp.newConditionalFormatRule()
+        .whenTextStartsWith("Failed")
+        .setBackground("#F4C7C3")  // red failure
+        .setRanges([calcStatusRange])
+        .build());
+    newRules.push(SpreadsheetApp.newConditionalFormatRule()
+        .whenFormulaSatisfied("=1")
+        .setBackground("#F4C7C3")  // red failure
+        .setRanges([calcStatusRange])
+        .build());
+    sheet.setConditionalFormatRules(newRules);
 }
