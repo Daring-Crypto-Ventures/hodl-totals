41efc6f9a01df8d18ecd4fa356a880a40786faee
Philip Rego <regop412@gmail.com>
2024-12-28 02:20:19 -0500

    fix too new error

--- src/validate.ts
+++ src/validate.ts
@@ -33,7 +33,9 @@ export default function validate(dateToLotAndSaleValues: LooselyTypedDataValidat
         } else {
             date = dateFromString(row[0]);
         }
-        if ((date.getTime() < oldestPossibleDate.getTime()) || (date.getTime() > now.getTime())) {
+        
+        const inputTimeLocal = new Date(date.getTime() - date.getTimezoneOffset() * 60000);
+        if ((date.getTime() < oldestPossibleDate.getTime()) || (inputTimeLocal.getTime() > now.getTime())) {
             return `Data Validation Error: Date is too old or too new on row ${rowIdx + 3}.`;
         }
         if (date.getTime() >= latestDate.getTime()) {
